13a44b47363d094ca45abb48929fa5bbc646fb0e
tubidge <tubidge@gmail.com>
2019-11-03 15:10:44 -0700

    comparisons practice

--- assets/app.js
+++ assets/app.js
@@ -11,3 +11,20 @@ console.log("Welcome back!");
 
 var nameLength = [...username];
 console.log(`Your name is ${nameLength.length} letters long.`);
+console.log(`~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~`);
+
+const dayStart = '07:30';
+const dayEnd = '17:00';
+
+function scheduleMeeting(startTime, durationMinutes) {
+    let endTime = startTime + durationMinutes;
+    if (startTime >= dayStart && (endTime) <= dayEnd) {
+        console.log('Hooray! Meeting can be scheduled.');
+    } else {
+        console.log("Sorry, we aren't available then.");
+    }
+    console.log(`Meeting end time: ${endTime}`);
+
+};
+
+scheduleMeeting('16:30', 60);
