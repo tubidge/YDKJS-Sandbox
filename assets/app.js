e80ad14ee49ba31fa0822fd04ddacdeeecbb6390
tubidge <tubidge@gmail.com>
2019-11-03 11:19:43 -0700

    file structure

---
+++ assets/app.js
@@ -0,0 +1,10 @@
+var username = prompt("What is your name?");
+
+if (username == "Phil") {
+    alert(`Welcome back Philly!`)
+} else {
+    alert(`Welcome back ${username}!`)
+};
+
+
+console.log("Welcome back!");
