3f731ba96c8f8eae0edd8bd27b769b2587f959ed
tubidge <tubidge@gmail.com>
2019-11-05 21:15:08 -0700

    comparisons time conversion

--- assets/app.js
+++ assets/app.js
@@ -9,22 +9,35 @@ if (username == "Phil") {
 
 console.log("Welcome back!");
 
+
 var nameLength = [...username];
 console.log(`Your name is ${nameLength.length} letters long.`);
 console.log(`~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~`);
 
+// YDKJS Chapter 2 Appendix A Practice
 const dayStart = '07:30';
 const dayEnd = '17:00';
 
 function scheduleMeeting(startTime, durationMinutes) {
+    // Convert dayStart, dayEnd, and startTime into comparable numbers
+    var startNums = dayStart.split(':');
+    var startHour = Number(startNums[0]);
+    var startMin = Number(startNums[1]);
+    console.log(startHour, startMin);
 
-    let endTime = startTime + durationMinutes;
-    if (startTime >= dayStart && (endTime) <= dayEnd) {
-        console.log('Hooray! Meeting can be scheduled.');
-    } else {
-        console.log("Sorry, we aren't available then.");
-    }
-    console.log(`Meeting end time: ${endTime}`);
+    var endNums = dayEnd.split(':');
+    var endHour = Number(endNums[0]);
+    var endMin = Number(endNums[1]);
+    console.log(endHour, endMin);
+
+
+    // let endTime = startTime + durationMinutes;
+    // if (startTime >= dayStart && (endTime) <= dayEnd) {
+    //     console.log('Hooray! Meeting can be scheduled.');
+    // } else {
+    //     console.log("Sorry, we aren't available then.");
+    // }
+    // console.log(`Meeting end time: ${endTime}`);
 
 };
 
